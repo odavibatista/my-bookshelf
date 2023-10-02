6d6c0830736c5e2434ae9297d3d5a84830238098
Davi Batista <823212382@ulife.com.br>
2023-10-02 20:03:06 -0300

    Entrega01(gui): building gui

---
+++ src/main/java/com/mycompany/_usjt_psc_sistema/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany._usjt_psc_sistema;
+
+/**
+ *
+ * @author 823212382
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
