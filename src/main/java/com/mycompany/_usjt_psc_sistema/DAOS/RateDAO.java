c3bd0b0126a1f0130ab690fccc893d72eb87e9eb
Davi Batista <davideosmar11@gmail.com>
2023-11-09 13:59:46 -0300

    DAO(Rate): added methods for verifications and deletions

--- src/main/java/com/mycompany/_usjt_psc_sistema/DAOS/RateDAO.java
+++ src/main/java/com/mycompany/_usjt_psc_sistema/DAOS/RateDAO.java
@@ -24,26 +24,32 @@ public class RateDAO {
         int userId = rate.getUserId();
         int rating = rate.getRate();
 
-        String sql = "INSERT INTO ratings (id, book_id, user_id, rating) VALUES (?, ?, ?, ?);";
-        var connection = ConnectionFactory.conectar();
-
-        PreparedStatement ps = connection.prepareStatement(sql);
-
-        ps.setInt(1, id);
-        ps.setInt(2, bookId);
-        ps.setInt(3, userId);
-        ps.setInt(4, rating);
-
-        int rowsAffected = ps.executeUpdate();
-
-        if (rowsAffected > 0) {
-            System.out.println("Avaliação cadastrada!");
+        // Check if the user has already rated the book
+        if (wasRated(rate)) {
+            JOptionPane.showMessageDialog(null, "Você já avaliou este livro.");
+            return;
         } else {
-            System.out.println("Erro ao cadastrar avaliação");
-        }
+            String sql = "INSERT INTO ratings (id, book_id, user_id, rating) VALUES (?, ?, ?, ?);";
+            var connection = ConnectionFactory.conectar();
 
-        ps.close();
-        connection.close();
+            PreparedStatement ps = connection.prepareStatement(sql);
+
+            ps.setInt(1, id);
+            ps.setInt(2, bookId);
+            ps.setInt(3, userId);
+            ps.setInt(4, rating);
+
+            int rowsAffected = ps.executeUpdate();
+
+            if (rowsAffected > 0) {
+                System.out.println("Avaliação cadastrada!");
+            } else {
+                System.out.println("Erro ao cadastrar avaliação");
+            }
+
+            ps.close();
+            connection.close();
+        }
     }
 
     /* Updating existing ratings */
@@ -67,5 +73,40 @@ public class RateDAO {
             JOptionPane.showMessageDialog(null, "Erro ao editar a sua avaliação. Tente novamente.");
             e.printStackTrace();
         }
+    }
+
+    /* Delete existing rating */
+    public void delete(int id) {
+        String sql = "DELETE FROM ratings WHERE id = ?;";
+        try (
+                var connection = ConnectionFactory.conectar();
+                var ps = connection.prepareStatement(sql);) {
+            ps.setInt(1, id);
+            ps.execute();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro ao deletar a avaliação. Tente novamente.");
+            e.printStackTrace();
+        }
+    }
+
+    /* Check if the user has already rated a book */
+    public boolean wasRated(Rate rate) throws Exception {
+        int userId = rate.getUserId();
+        int bookId = rate.getBookId();
+        String sql = "SELECT * FROM ratings WHERE user_id = ? AND book_id = ?";
+
+        try (var connection = ConnectionFactory.conectar();
+                var ps = connection.prepareStatement(sql)) {
+            ps.setInt(1, userId);
+            ps.setInt(2, bookId);
+            var rs = ps.executeQuery();
+            if (rs.next()) {
+                return true;
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro ao verificar a avaliação. Tente novamente.");
+            e.printStackTrace();
+        }
+        return false;
     }
 }
